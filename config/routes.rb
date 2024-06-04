079ee3a197015ba0cfc588fb6c6e3c90f55f0b2c
Elaine Naomi <elaine.n.watanabe@gmail.com>
2024-06-04 11:31:53 -0300

    Fix gemfile.lock + .gitignore (#5)
    * Update gemfile.lock and .gitignore
    
    * breaking a test on purpose
    
    * Linter
    
    * Fix CI 🫶🏻

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-require 'sidekiq/web'
-require 'sidekiq-scheduler/web'
+require "sidekiq/web"
+require "sidekiq-scheduler/web"
 
 Rails.application.routes.draw do
   resources :mentees
@@ -7,15 +7,15 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', :as => :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
-                                                ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
-                                                  ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+        ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
   end
-  mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => "/sidekiq"
 
   # Defines the root path route ("/")
   # root "posts#index"
